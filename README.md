b7a74b78a927d049c1c47263e0b91d241d969d50
Sahil Lamba <sahil.lamba95@gmail.com>
2021-09-03 01:57:11 +0530

    Create test VPC, add npm scripts with AWS profile

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # samridhdhi-infra
+
+## Commands
+
+### Setup
+1. `npm run setup && npm run cdk:setup`
+
+### Usage
+1. `npm run build`
+1. `npm run cdk:synth`
+1. `npm run cdk:bootstrap` (one-time per region)
+1. `npm run cdk:deploy`
--- lib/samridhdhi-infra-stack.ts
+++ lib/samridhdhi-infra-stack.ts
@@ -1,9 +1,10 @@
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 
 export class SamridhdhiInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Test-VPC');
   }
 }
